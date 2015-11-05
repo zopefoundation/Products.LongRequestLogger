4a866b4c289a6242615eaffefe3d16759deab480
Leonardo Rochael Almeida <leorochael@gmail.com>
2015-11-05 11:53:30 -0200

    Simplify the monitor
    Dump all "timeouted" threads at once, them sleep for `interval`.
    
    Or sleep for `timeout` if no threads left to monitor.

--- Products/LongRequestLogger/monitor.py
+++ Products/LongRequestLogger/monitor.py
@@ -4,13 +4,16 @@
 #
 ##############################################################################
 
-import os, threading
-from collections import deque
-from select import select
+import os
+import threading
+import sys
+
+from collections import OrderedDict
 from thread import get_ident
-from time import time
+from time import time, sleep
 from .dumper import Dumper
 
+TOO_LONG_MSG = "dumping threads took %.2f seconds, longer than interval (%.2f)"
 
 class Monitor(threading.Thread):
     """Logs the stack-trace of a thread until it's stopped
@@ -20,80 +23,41 @@ class Monitor(threading.Thread):
         assert interval > 0, interval
         threading.Thread.__init__(self)
         self.log = log
-        self.timeout = timeout
+        self.timeout = max(timeout, interval)
         self.interval = interval
-        self.lock = threading.Lock()
-        self.dumpers = deque()
-        self.event_pipe = os.pipe()
+        self.dumpers = OrderedDict()
         self.daemon = True
+        self.stopped = False
         self.start()
 
     def stop(self):
-        with self.lock:
-            if not self.event_pipe:
-                return
-            os.close(self.event_pipe[1])
-            self.event_pipe = None
+        self.stopped = True
         self.join()
 
     def run(self):
-        r = self.event_pipe[0]
-        try:
-            l = self.lock
-            rlist = r,
-            dumpers = self.dumpers
-            t = 0
-            while not select(rlist, (), (), t)[0] or os.read(r, 8):
-                if dumpers:
-                    dumper = None
-                    try:
-                        while 1:
-                            with l:
-                                now = time()
-                                if dumper is dumpers[0]:
-                                    del dumpers[0]
-                                    dumper.next_dump = now + self.interval
-                                    self.push(dumper)
-                                dumper = dumpers[0]
-                            t = dumper.next_dump - now
-                            if t > 0:
-                                break
-                            # We released the lock to not block other threads
-                            # when logging. In particular, format_thread()
-                            # sometimes takes several seconds to process.
-                            msg = dumper.format_thread()
-                            if dumper is dumpers[0]:
-                                self.log.warning(msg)
-                        continue
-                    except IndexError: # dumpers empty
-                        pass
-                t = None
-        finally:
-            os.close(r)
-
-    def push(self, dumper):
         dumpers = self.dumpers
-        i = j = len(dumpers)
-        while i and dumper.next_dump < dumpers[-1].next_dump:
-            dumpers.rotate()
-            i -= 1
-        dumpers.append(dumper)
-        dumpers.rotate(i - j)
-        return i
+        while not self.stopped:
+            start_time = time()
+
+            for thread_id, dumper in list(dumpers.items()):
+                if dumper is not None and time() - dumper.start > self.timeout:
+                    msg = dumper.format_thread()
+                    # format_thread could take some time and that dumper
+                    # could be gone by now (i.e. the request finished)...
+                    if dumpers.get(thread_id) is dumper:
+                        self.log.warning(msg)
+
+            elapsed = start_time - time()
+            sleep_time = (self.interval if dumpers else self.timeout) - elapsed
+            if sleep_time > 0:
+                sleep(sleep_time)
+            else:
+                self.log.error(TOO_LONG_MSG, elapsed, self.interval)
 
     def __enter__(self):
-        with self.lock:
-            dumper = Dumper()
-            dumper.next_dump = dumper.start + self.timeout
-            if not self.push(dumper) and self.event_pipe:
-                os.write(self.event_pipe[1], '\0')
+        thread_id = get_ident()
+        self.dumpers[thread_id] = Dumper(thread_id)
 
     def __exit__(self, t, v, tb):
-        with self.lock:
-            thread_id = get_ident()
-            for i, dumper in enumerate(self.dumpers):
-                if dumper.thread_id == thread_id:
-                    del self.dumpers[i]
-                    if not i and self.event_pipe:
-                        os.write(self.event_pipe[1], '\0')
-                    break
+        thread_id = get_ident()
+        del self.dumpers[thread_id]
